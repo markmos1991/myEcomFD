d11f84f33510ed17413ddd1d974296fb030684da
markmos1991 <101826190+markmos1991@users.noreply.github.com>
2023-05-30 17:06:54 +0100

    changed component name

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import '../styles/globals.css';
 import { Inter } from 'next/font/google';
 
-import Header from '@/components/header';
+import Header from '@/components/Header';
 import Footer from '@/components/Footer';
 import HeroSection from '@/components/HeroSection';
 import Content from '@/components/Content';
